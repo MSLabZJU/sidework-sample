e02727bd78f1584c881f7c76e7c11cfa69fe599d
浙大 MSL 实验室 <48726698+MSLabZJU@users.noreply.github.com>
2019-03-20 21:26:05 +0800

    接下来的一段时间的任务分配

--- SideworkDemo.java
+++ SideworkDemo.java
@@ -2,5 +2,11 @@ public class SideworkDemo{
 	public static void main(String[] args) {
 		// A 童鞋添加了这一句
 		System.out.println("AAA");
+
+		// B 同学请在这里添加 PDR 的代码
+		
+		// C 同学请在这里补充 地图显示 的代码
+		
+		// D 同学请在这里添加 定位算法 模块
 	}
 }
